a676be8e3d4c0f8953cc9ea3a58e571441c3a114
Liu Changyu <liuchangyu1111@gmail.com>
2020-08-11 13:11:17 +0800

    Format code

--- include/common.h
+++ include/common.h
@@ -1,118 +1,96 @@
 /*
-  * Copyright (c) 2020, Lorna Authors. All rights reserved.
-  *
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  *     http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  */
+ * Copyright (c) 2020, Lorna Authors. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #ifndef LENET_COMMON_H
 #define LENET_COMMON_H
 
-#include "NvInfer.h"
-#include <iostream>
 #include <ctime>
+#include <iostream>
+#include "NvInfer.h"
 
-#define CHECK(status)                                   \
-    do                                                                         \
-    {                                                                             \
-        auto ret = (status);                                     \
-        if (ret != 0)                                                      \
-        {                                                                          \
-            std::cout << "Cuda failure: " << ret; \
-            abort();                                                        \
-        }                                                                          \
-    } while (0)
+#define CHECK(status)                       \
+  do {                                      \
+    auto ret = (status);                    \
+    if (ret != 0) {                         \
+      std::cout << "Cuda failure: " << ret; \
+      abort();                              \
+    }                                       \
+  } while (0)
 
-constexpr long double operator"" _GiB(long double val)
-{
-    return val * (1 << 30);
-}
-constexpr long double operator"" _MiB(long double val)
-{
-    return val * (1 << 20);
-}
-constexpr long double operator"" _KiB(long double val)
-{
-    return val * (1 << 10);
-}
+constexpr long double operator"" _GiB(long double val) { return val * (1 << 30); }
+constexpr long double operator"" _MiB(long double val) { return val * (1 << 20); }
+constexpr long double operator"" _KiB(long double val) { return val * (1 << 10); }
 
 // These is necessary if we want to be able to write 1_GiB instead of 1.0_GiB.
 // Since the return type is signed, -1_GiB will work as expected.
-constexpr long long int operator"" _GiB(long long unsigned int val)
-{
-    return val * (1 << 30);
-}
-constexpr long long int operator"" _MiB(long long unsigned int val)
-{
-    return val * (1 << 20);
-}
-constexpr long long int operator"" _KiB(long long unsigned int val)
-{
-    return val * (1 << 10);
-}
+constexpr long long int operator"" _GiB(long long unsigned int val) { return val * (1 << 30); }
+constexpr long long int operator"" _MiB(long long unsigned int val) { return val * (1 << 20); }
+constexpr long long int operator"" _KiB(long long unsigned int val) { return val * (1 << 10); }
 
 // Logger for TensorRT info/warning/errors
 class Logger : public nvinfer1::ILogger {
-public:
+ public:
+  Logger() : Logger(Severity::kWARNING) {}
 
-    Logger() : Logger(Severity::kWARNING) {}
+  Logger(Severity severity) : reportableSeverity(severity) {}
 
-    Logger(Severity severity) : reportableSeverity(severity) {}
+  void log(Severity severity, const char *msg) override {
+    // suppress messages with severity enum value greater than the reportable
+    if (severity > reportableSeverity) return;
 
-    void log(Severity severity, const char *msg) override {
-        // suppress messages with severity enum value greater than the reportable
-        if (severity > reportableSeverity) return;
-
-        switch (severity) {
-            case Severity::kINTERNAL_ERROR:
-                std::cerr << "[INTERNAL_ERROR]: ";
-                break;
-            case Severity::kERROR:
-                std::cerr << "[ERROR]: ";
-                break;
-            case Severity::kWARNING:
-                std::cerr << "[WARNING]: ";
-                break;
-            case Severity::kINFO:
-                std::cout << "[INFO]: ";
-                break;
-            default:
-                std::cout << "[UNKNOWN]: ";
-                break;
-        }
-        std::cout << msg << std::endl;
+    switch (severity) {
+      case Severity::kINTERNAL_ERROR:
+        std::cerr << "[INTERNAL_ERROR]: ";
+        break;
+      case Severity::kERROR:
+        std::cerr << "[ERROR]: ";
+        break;
+      case Severity::kWARNING:
+        std::cerr << "[WARNING]: ";
+        break;
+      case Severity::kINFO:
+        std::cout << "[INFO]: ";
+        break;
+      default:
+        std::cout << "[UNKNOWN]: ";
+        break;
     }
+    std::cout << msg << std::endl;
+  }
 
-    Severity reportableSeverity{Severity::kWARNING};
+  Severity reportableSeverity{Severity::kWARNING};
 };
 
 void printMessage(unsigned int LEVEL) {
-    std::time_t now = time(0);
-    std::tm *ltm = localtime(&now);
-    switch (LEVEL)
-    {
+  std::time_t now = time(0);
+  std::tm *ltm = localtime(&now);
+  switch (LEVEL) {
     case 0:
-        std::cout  << "[INFO(" << ltm->tm_hour <<":" << ltm->tm_min <<  ":" << ltm->tm_sec << ")]: ";
-        break;
+      std::cout << "[INFO(" << ltm->tm_hour << ":" << ltm->tm_min << ":" << ltm->tm_sec << ")]: ";
+      break;
     case 1:
-        std::cout << "[WARNING(" << ltm->tm_hour << ":" << ltm->tm_min <<  ":" << ltm->tm_sec << ")]: ";
-        break;
+      std::cout << "[WARNING(" << ltm->tm_hour << ":" << ltm->tm_min << ":" << ltm->tm_sec << ")]: ";
+      break;
     case 2:
-        std::cerr << "[ERROR(" << ltm->tm_hour << ":" << ltm->tm_min <<  ":" << ltm->tm_sec << ")]: ";
-        break;
+      std::cerr << "[ERROR(" << ltm->tm_hour << ":" << ltm->tm_min << ":" << ltm->tm_sec << ")]: ";
+      break;
     default:
-        std::cout << "[INFO(" << ltm->tm_hour << ":" << ltm->tm_min <<  ":" << ltm->tm_sec << ")]: ";
-        break;
-    }
+      std::cout << "[INFO(" << ltm->tm_hour << ":" << ltm->tm_min << ":" << ltm->tm_sec << ")]: ";
+      break;
+  }
 }
 
-#endif // LENET_COMMON_H
+#endif  // LENET_COMMON_H
